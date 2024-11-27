b42d35885791e59ad1261a4fd6320314b1ff593c
Abdelazim Ramadan <Abdelazim Ramadan>
2024-11-27 19:18:03 +0200

    Solve LeetCode problem 242: Valid Anagram.

---
+++ leet_code/242_Valid_Anagram.dart
@@ -0,0 +1,21 @@
+import 'dart:collection';
+import 'dart:io';
+
+void main() {
+  String s = stdin.readLineSync()!;
+  String t = stdin.readLineSync()!;
+  print(Solution().isAnagram(s, t));
+}
+
+class Solution {
+  String sortString(String input) {
+    return (input.split('')..sort()).join('');
+  }
+
+  bool isAnagram(String s, String t) {
+    if (s.length != t.length) {
+      return false;
+    }
+    return sortString(s) == sortString(t);
+  }
+}
